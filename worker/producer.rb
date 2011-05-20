ef05a33313b5d865c32d6c6b431b78b56ad41001
John F. Miller <emperor@antarestrader.com>
2011-05-19 20:56:26 -0700

    producer consumer

--- worker/producer.rb
+++ worker/producer.rb
@@ -1,13 +1,20 @@
 require "rubygems"
 require "amqp"
+require "json"
 
-begin
-  AMQP.start({
+REMOTE = {
+      port: 1419,
       host: "184.73.224.102", 
       username: 'imagepu_hack', 
       password: 'userhack1295', 
-      vhost: 'imagepu_queue'}) do
-    q = MQ.queue("worker_test").publish "hello world"
+      vhost: 'imagepu_queue'}
+      
+LOCAL = {host: "localhost"}
+
+begin
+  AMQP.start(LOCAL) do
+    MQ.new.queue("worker_test").publish({name: "hello world", date: "today"}.to_json)
+    AMQP.stop { EM.stop }
   end
 rescue AMQP::Error => e
   puts e
--- worker/worker.rb
+++ worker/worker.rb
@@ -1,15 +1,19 @@
 require "rubygems"
 require "amqp"
+require "json"
 
+REMOTE = {
+      port: 1419,
+      host: "184.73.224.102", 
+      username: 'imagepu_hack', 
+      password: 'userhack1295', 
+      vhost: 'imagepu_queue'}
+      
+LOCAL = {host: "localhost"}
 
-
-AMQP.start({
-    host: "184.73.224.102", 
-    username: 'imagepu_hack', 
-    password: 'userhack1295', 
-    vhost: 'imagepu_queue'}) do
+AMQP.start(LOCAL) do
   q = MQ.new.queue("worker_test")
   q.subscribe do |message|
-    puts message
+    puts JSON.parse(message)
   end
 end
